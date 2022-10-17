5aa69e1400ef9efa44e98f43b7de3329aece6b58
Anil7381 <115988439+Anil7381@users.noreply.github.com>
2022-10-17 15:40:53 +0530

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <h1><center>USER LOGIN</center></h1>
+    
+    <form id="form1" runat="server">
+        <div><center>
+            <asp:Table runat="server" BorderStyle="Groove" CellPadding="10" CellSpacing="5" Font-Bold="True" Font-Overline="False" HorizontalAlign="Justify" Visible="True" Font-Strikeout="False" Font-Size="20" GridLines="None" Font-Italic="False">
+        <asp:TableHeaderRow>
+            <asp:TableCell>UserName</asp:TableCell>
+            <asp:TableCell><asp:TextBox ID="Textbox1" runat="server"></asp:TextBox></asp:TableCell>
+        </asp:TableHeaderRow>
+        <asp:TableRow>
+            <asp:TableCell>Password</asp:TableCell>
+            <asp:TableCell><asp:TextBox ID="Textbox2" runat="server"></asp:TextBox></asp:TableCell>
+        </asp:TableRow>          
+    </asp:Table>
+             </center>
+            <br />
+              <center>
+                  <asp:Button ID="Button1" runat="server" Text="LOGIN" Font-Bold="True" BorderWidth="2" Font-Size="15" OnClick="Button1_Click" /></center>  
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class _Default : System.Web.UI.Page
+{
+    SqlConnection conn = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MiniProject; Integrated Security=SSPI");
+    SqlCommand cmd = new SqlCommand();
+    SqlDataAdapter adp=new SqlDataAdapter();    
+    
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        DataTable dt = new DataTable();
+        cmd.CommandText = "Select * from Users Where UserName='" + Textbox1.Text + "' and UserPassword='" + Textbox2.Text + "'";
+        cmd.Connection = conn;
+        conn.Open();
+        adp.SelectCommand = cmd;
+        adp.Fill(dt);
+        conn.Close();
+        if (dt.Rows.Count > 0)
+        {
+            Response.Write("UserId :"+dt.Rows[0][0].ToString());
+            Response.Write("<br/>");
+            Response.Write("UserName :"+dt.Rows[0][1].ToString());
+            Response.Write("<br/>");
+            Response.Write("UserPassword :" + dt.Rows[0][2].ToString());
+            Response.Write("<br/>");
+            Response.Write("UserAddress :" + dt.Rows[0][3].ToString());
+
+        }
+        else
+        {
+            Response.Write("Incorrect User login credentials");
+        }
+
+    }
+
+    
+}
